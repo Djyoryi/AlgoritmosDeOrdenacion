c62e36a9d431a81a8dd5fd05ecfa0acb8c11d098
fecq8 <fecq8@172.16.0.98>
2019-04-25 16:37:19 +0200

    haciendo 2metodo

--- src/burbujadeordenacion/BurbujaDeOrdenacion.java
+++ src/burbujadeordenacion/BurbujaDeOrdenacion.java
@@ -13,18 +13,49 @@ package burbujadeordenacion;
 
 
 public class BurbujaDeOrdenacion {
+    
+    
        
-    public void metodoBurbuja (int [] numeros){
-        for (int j=1; j < numeros.length; j++){
-           for (int i=1; i < numeros.length - j; i++){
-               if (numeros[i] < numeros[i+1]){
-                   i++;
-               }else{
-                   numeros[i] = numeros[i+1];
+    public int[] metodoBurbuja (int [] numeros){
+        
+        int [] listaBurbuja = {numeros[0], numeros[1]};
+        int aux = 0;
+        for (int j=0; j < numeros.length; j++){
+           for (int i=0; i < numeros.length - 1; i++){
+               if (numeros[i] > numeros[i+1]){
+                   aux = numeros[i+1];
                    numeros[i+1] = numeros[i];
+                   numeros[i] = aux;
+                   
                }
            } 
         }
+        return numeros;
+    }
+    
+    public int[] metodoInserccion (int [] numeros){
+        int z = 0;
+        int aux = 0;
+        for (int j=0; j < numeros.length; j++){
+           for (int i=1; i < numeros.length -1; i++){
+               if (numeros[i] < numeros[0]){
+                   numeros[0] = aux;
+                    numeros[0] = numeros[i];
+               }else{
+                   while (numeros[i] <  numeros.length && numeros[i] > numeros[z]){
+                      
+                       z++;
+                       
+                   }
+                   aux = numeros[z];
+                   numeros [z]= numeros[i+1];
+                   
+                   
+               }
+           }
+    
+    }
+        return numeros;
     }
     
 
@@ -33,8 +64,18 @@ public class BurbujaDeOrdenacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] numeros = {22,17,21,55,13,24};
+        BurbujaDeOrdenacion ejercicio = new BurbujaDeOrdenacion();
         
-        int[] listaNumeros = {11,17,21,55};
+        ejercicio.metodoBurbuja(numeros);
+        for (int i=0; i < numeros.length ; i++){
+            System.out.println(" " + numeros[i]);
+        }
+        
+        ejercicio.metodoInserccion(numeros);
+        for (int i=0; i < numeros.length ; i++){
+            System.out.println("   " + numeros[i]);
+        }
     }
     
 }
